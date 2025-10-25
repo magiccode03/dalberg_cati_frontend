3a6be18d52d3d90e4c73ab29ba139f9acbcdd6ff
“cktriline” <chandrakamal@triline.co.in>
2025-10-25 18:18:31 +0530

    update: teleform and qc form

--- src/app/cati/ss/tele-form/[id]/[ac_code]/page.tsx
+++ src/app/cati/ss/tele-form/[id]/[ac_code]/page.tsx
@@ -479,7 +479,7 @@ export default function TeleFormV2Page() {
         },
         body: JSON.stringify({
           ...transformedData,
-          status: 4, // Draft status
+          status: formData.thanks_future == '1' || formData.thanks_future == '2' ? 2 : 4, // Draft status
           form_duration_seconds: timer,
           language_used: language,
         })
