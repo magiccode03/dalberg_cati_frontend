bc35ce7cfab46c73d3857c49fa79a71d43a57ff9
“cktriline” <chandrakamal@triline.co.in>
2025-10-28 09:09:53 +0530

    fix: telecaller progress page table

--- src/app/cati/ppm/progress/telecaller-progress/page.tsx
+++ src/app/cati/ppm/progress/telecaller-progress/page.tsx
@@ -1494,7 +1494,7 @@ const TelecallerProgressPage: React.FC = () => {
                         <th className="px-4 py-3 font-semibold text-gray-700 text-center">Caller Mobile No.</th>
                         <th className="px-4 py-3 font-semibold text-gray-700 text-center">Number of Dials</th>
                         <th className="px-4 py-3 font-semibold text-gray-700 text-center">Number of Calls Connected</th>
-                        <th className="px-4 py-3 font-semibold text-gray-700 text-center">Talk Duration</th>
+                        <th className="px-4 py-3 font-semibold text-gray-700 text-center">Form Duration</th>
                         <th className="px-4 py-3 font-semibold text-gray-700 text-center">Call Not Received to Telecaller</th>
                         <th className="px-4 py-3 font-semibold text-gray-700 text-center">Ringing</th>
                         <th className="px-4 py-3 font-semibold text-gray-700 text-center">Not Ringing</th>
